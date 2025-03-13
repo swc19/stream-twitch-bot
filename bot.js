b89cc2d42b9854778db89e6cd8fe49559db10437
swc19 <swc6992@rit.edu>
2025-03-13 13:54:27 -0500

    bot changes

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ const COMMAND_START_CHAR = "!"
 const CUSTOM_TEXT_COMMANDS = {
     // simple commands that exclusively return a string, does not take in any context or api
     "discord": {
-        "text": "My Discord can be found here: discord.gg/35dfPZY",
+        "text": "My Discord can be found here: https://discord.gg/35dfPZY",
         "enabled": true
     },
     "donate": {
@@ -41,14 +41,18 @@ const CUSTOM_TEXT_COMMANDS = {
     },
     "twitter": {
         "text": "Follow me on Twitter: https://twitter.com/_swooce",
-        "enabled": true
+        "enabled": false
     },
+	"bluesky": {
+		"text": "Follow me on Bluesky: https://bsky.app/profile/swooce.bsky.social",
+		"enabled": true
+	},
     "geoguessr": {
         "text": "My Geoguessr profile is here: https://www.geoguessr.com/user/60d1134800ea7b000152a728, and you can sign up with my link here and save 40%! https://www.geoguessr.com/referral-program/CO78-SFGL-6R3Q",
         "enabled": true
     },
     "flags": {
-        "text": "My progress on contacting every state, province and country for a flag/license plate is here: http://tinyurl.com/swooceFlagProject",
+        "text": "Here's a list of all the flags that I have: https://docs.google.com/spreadsheets/d/1YjFjc2HcvzzsHm81e5iJXoqErgYdAgafyqTCZkPpqLM/edit?usp=sharing",
         "enabled": false
     },
     "confidence": {
@@ -56,24 +60,58 @@ const CUSTOM_TEXT_COMMANDS = {
 		"enabled": false
 	},
     "gcl": {
-		"text": "I'm in Division 7 in the Geoguessr Challenge League for Season 5! https://docs.google.com/spreadsheets/d/1LDuySvElv31l-uq37d7215PG1Ew2C88d4ahx209MvjU/edit?usp=sharing",
+		"text": "I'm in Division 3 in the Geoguessr Challenge League for Season 14! https://docs.google.com/spreadsheets/d/1LDuySvElv31l-uq37d7215PG1Ew2C88d4ahx209MvjU/edit?gid=795358607#gid=795358607",
 		"enabled": true
-	}
+	},
+	"map": {
+		"text": "I made a Geoguessr map of some of the most unique towns in the USA! https://www.geoguessr.com/maps/669680d6170a4d3d9cddc64a",
+		"enabled": true
+	},
+	"playlist": {
+		"text": "https://open.spotify.com/playlist/13goTpg7eJ7FdjOtNU4iFs?si=efdb1d3b686a43d1",
+		"enabled": true
+	},
+	"deconstructed": {
+		"text": "I have a YouTube series that will eventually cover every country in GeoGuessr! https://www.youtube.com/playlist?list=PLBVB4CuFCGsc85oahk4Ba1D-ZazKFLO3X",
+		"enabled": false
+	},
+	"youtube": {
+		"text": "Check out my YouTube channel! https://www.youtube.com/@swooce_",
+		"enabled": true
+	},
+	"newvideo": {
+		"text": "My most recent Geoguessr Deconstructed video is on Mexico: https://youtu.be/o_0j1hWfiP0",
+		"enabled": false
+	},
+	"lithuania": {
+		"text": "https://www.twitch.tv/gamesdonequick/clip/GracefulSillyCheddarRitzMitz-URT53wAJWfSYor8b",
+		"enabled": true
+	},
+	"gdq": {
+		"text": "Check out my AGDQ25 run VODs! https://www.youtube.com/watch?v=-q-dIKUXH90 & https://www.youtube.com/watch?v=0E07MLrrw5A",
+		"enabled": true
+	},
+	"500k": {
+		"text": "There's no GCL for March, but I'm doing a 100 seed 5k speedrun, hosted by the same group. More info at https://imgur.com/a/er6udII",
+		"enabled": true
+	},
+    "gim": {
+        "text": "I'm in an OSRS competition for the month of April! Rules and info here: https://docs.google.com/document/d/1zRCrJ9eDA5ZIdNGMhmC5kcip-r_av_4RvuwvFLVmK1I/edit?tab=t.0",
+        "enabled": false
+    }
+}
+const ALIASES = {
+	"deconstruct": "deconstructed",
+	"yt": "youtube",
+	"agdq": "gdq",
+	"bsky": "bluesky"
 }
 
 const CHATGUESSR_COMMANDS = ["cg", "cgflags", "best", "me", "clear", "randomplonk"]
 // cooldowns in seconds
 // cooldowns are currently global for all users, to avoid spam
 const COOLDOWNS = {
-    // text commands
-    "discord": 30,
-    "donate": 30,
-    "twitter": 30,
-    "geoguessr": 30,
-    "lurk": 30,
-    "flags": 30,
-    "gcl": 30,
-    
+	// default text cooldown is 30s
     // api/function based commands
     "followage": 300,
     
@@ -99,32 +137,41 @@ async function onMessageHandler(target, context, msg, self) {
             return 1;
         }
     }
-
+	const isCommand = (command) => {
+        return Object.keys(CUSTOM_TEXT_COMMANDS).includes(command) || Object.keys(ALIASES).includes(command);
+    }
     const [first, ...second] = msg.split(" ");
     // ...second is [] of each word after it, even if it's one word
 
     // returns the command itself in a normalized fashion
-    const command = first.slice(1).toLowerCase();
+    let command = first.slice(1).toLowerCase();
 
     // ignore any commands that pertain to the chatguessr bot
     // just make sure to not make a custom command that's one of those words
 	if(CHATGUESSR_COMMANDS.includes(command)){ return; }
 
     if(first.startsWith(COMMAND_START_CHAR)){
+
         const off_cooldown = () => {
+			const cooldown_time = 30; // default text command cooldown
             // check if the given command is on cooldown
             if(COOLDOWNS[command]){
-                // convert to seconds with the /1000
-                return (Date.now() - last_used[command])/1000 > COOLDOWNS[command]
+				cooldown_time = COOLDOWNS[command];
             } else {
                 return true;
             }
+			// convert to seconds with the /1000
+			return (Date.now() - last_used[command])/1000 > cooldown_time;
         }
+
         if(USER_PERMS() >= 3 || !last_used[command] || (last_used[command] && off_cooldown())){
             // currently, mods and streamer can use any command off cooldown
-            if(Object.keys(CUSTOM_TEXT_COMMANDS).includes(command)){
+            if(isCommand(command)){
                 // verify that the command is active and valid
-                if(CUSTOM_TEXT_COMMANDS[command].enabled) {
+				if(Object.keys(ALIASES).includes(command)){
+					command = ALIASES[command];
+				}
+                if(CUSTOM_TEXT_COMMANDS[command].enabled || !last_used[command] || (last_used[command] && off_cooldown())) {
                     client.say(target, CUSTOM_TEXT_COMMANDS[command].text);
                     console.log(`* Executed ${first} for user ${context.username}`)
                     // add the command and the current timestamp to the list
